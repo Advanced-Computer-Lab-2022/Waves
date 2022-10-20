5c59494a2ec829409e12352b7b63c4b34e4828a9
Adam Samy <manrandy1@hotmail.com>
2022-10-20 19:17:49 +0200

    Server Started

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const express = require('express');
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.listen(app.get("port"), function(){
+    console.log('Server Started On Port ' + app.get('port'))
+});
