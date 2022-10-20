170f94bbc84220b2afc613ed0237c541be57a9d7
Adam Samy <manrandy1@hotmail.com>
2022-10-20 19:28:46 +0200

    Added Routes File
    Linked Between App.js And Routes.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var path = require("path");
 
+var routes = require("./routes");
 var app = express();
 
+app.use(routes);
 app.set("port", process.env.PORT || 3000);
 
 app.listen(app.get("port"), function(){
