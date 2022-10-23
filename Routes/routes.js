225ad7ba235500711d8b5f2507bdf5a56789b135
Adam Samy <manrandy1@hotmail.com>
2022-10-23 19:41:13 +0200

    Improved sign-up, login and added admin page

--- Routes/routes.js
+++ Routes/routes.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req,res){
-    res.render("index");
+    res.render("home");
 });
 
 router.get("/sign-up", function(req,res){
@@ -19,7 +19,21 @@ router.post("/sign-up", function(req,res){
 });
 
 router.post("/login", function(req,res){
-    res.render("login");
+    res.render("login", {
+        err: ''
+    })
+});
+
+router.post("/administrator", function(req,res){
+    res.render("admin");
+});
+
+router.post("/add-administrator", function(req,res){
+    res.render("add-admin");
+});
+
+router.post("/add-instructor", function(req,res){
+    res.render("add-instructor");
 });
 
 module.exports = router
