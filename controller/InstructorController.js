813603f7fe82a6cdd1608db6819bfd903b51107c
Zeyad-01 <108385626+Zeyad-01@users.noreply.github.com>
2022-11-28 05:04:27 -0800

    Revert "backend twentyone,twentyeight, thirtythree and thirtyfour"
    This reverts commit 659840344f8148addc9920beb9d5f579ac76409e.

--- controller/InstructorController.js
+++ controller/InstructorController.js
@@ -1,8 +1,7 @@
 //const mongoose = require("mongoose");
-//var router = express.Router();
+//let router = express.Router();
 
 const Courses = require("../models/Courses");
-const Instructor = require("../models/Instructor");
 
 function addCourse (body, username){
     const newCourse =new Courses({
@@ -21,52 +20,6 @@ function addCourse (body, username){
 async function getMyCourses (courses, username){
     return courses.filter(item => item.givenBy==username);
 }
-
-async function getMyCoursesRatings (courses, username){
-const coursesratings={};
-filteredcourses= courses.filter(item => item.givenBy==username);
-filteredcourses.forEach(course => {coursesratings.push(course.courseRating)
-    
-});
-
-
-}
-async function getMyCoursesReviews (courses, username){
-const coursesreviews={};
-filteredcourses.filter(item => item.givenBy==username);
-filteredcourses.forEach(course => {coursesreviews.push(course.courseReviews)
-    
-});
-
-
-}
-
-async function getMyRatings (instructor, username){
-    const instructorratings={};
-     filteredinstructors.filter(item => item.givenBy==username);
-     filteredinstructors.forEach(instructor1 => {instructorratings.push(Instructor.instructorRatings)
-    
-});
-
-
-}
-async function getMyReviews (instructor, username){
-    const instructorreviews={};
-filteredinstructors.filter(item => item.givenBy==username);
-filteredinstructors.forEach(instructor1 => {instructorreviewss.push(Instructor.instructorReviews)
-    
-});
-
-
-}
-
-
-
-
-
-
-
-
 
 module.exports= {addCourse, getMyCourses};
     //res.render("guest");
