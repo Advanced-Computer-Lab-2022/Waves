6e5ca0c3d91ba0e2e685f5d2fa8adc21a918dde2
Adam Samy <manrandy1@hotmail.com>
2022-11-28 06:31:58 +0200

    Course Component

---
+++ frontend/front/src/Course.tsx
@@ -0,0 +1,28 @@
+import {
+    BrowserRouter as Router,
+    Route,
+    useNavigate,
+    Link
+  } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+interface props {
+    CourseName: string;
+    Course: string;
+    CourseName: string;
+    CourseName: string;
+    CourseName: string;
+    CourseName: string;
+    children?: React.ReactNode;
+    customChild?: React.ReactNode;
+}
+
+const Course = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Dummy
