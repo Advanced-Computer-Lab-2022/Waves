f5d84cd162750b0be80e41882bac2d4fa19123ad
Zeyad-01 <108385626+Zeyad-01@users.noreply.github.com>
2022-12-03 02:16:53 -0800

    updated exam session

--- frontend/src/pages/ExamSession.tsx
+++ frontend/src/pages/ExamSession.tsx
@@ -12,22 +12,42 @@ import {useLocation, useNavigate} from "react-router-dom";
 import { response } from 'express';
 
 const ExamSession = (props:any) => {
+    const fifo=[];
+    fifo.push(1);
     //const navigate = useNavigate();
     const [examsQuestions, setExamQuestions] = React.useState<any[]>([]);
+    const [values, setValue] = React.useState('');
+    //var [score, setScore] = React.useState(0);
+    const [error, setError] = React.useState(false);
+    const [helperText, setHelperText] = React.useState('Choose wisely');
     const location = useLocation();
     const data = location.state?.data;
+    var score=0;
+   
+    const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValue((event.target as HTMLInputElement).value);
+      setHelperText(' ');
+      setError(false);
+    };
     // var s1;
     // var s2;
     // var s3;
     // var s4;
     const handleSubmit = (e: any) => {
         e.preventDefault();
-        const userInfo = new FormData(e.target)
-         const s1 = Object.fromEntries(userInfo.entries()).A
-         const s2 = Object.fromEntries(userInfo.entries()).B
-         const s3 = Object.fromEntries(userInfo.entries()).C
-         const s4 = Object.fromEntries(userInfo.entries()).D
-         axios.post('http://localhost:3001/exam-session', {});
+         
+        axios.post('http://localhost:3001/exam-session', {
+           // solution : value
+            
+          }, {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            }
+          })
+            .then(response => {
+                console.log(values)
+            });
+       
     }
     axios.post('http://localhost:3001/exam-session', {
             belongsToCourse: data[0] ,
@@ -45,38 +65,54 @@ const ExamSession = (props:any) => {
         <>
         <ResponsiveNavBar/>
         
-        <Stack marginTop={0.6} direction={"row"}>
-                <FilterBar/>
+        <Stack marginTop={0.6} direction="column">
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 15 }}>
                     {examsQuestions && examsQuestions.map((examsQuestion) => (
                         <><Grid marginTop={2} item xs={2} sm={4} md={4} key={examsQuestion._id}>
-                            question: {examsQuestion.question} A: {examsQuestion.c1} B: {examsQuestion.c2} C: {examsQuestion.c3} D: {examsQuestion.c4}
-                        </Grid>
-                        <form onSubmit={handleSubmit}>
+                            <Stack direction="column" marginLeft={2}>
+                            question: {examsQuestion.question} 
+                            <div/>
+                            A: {examsQuestion.c1}
+                            <div/> 
+                            B: {examsQuestion.c2}
+                            <div/> 
+                            C: {examsQuestion.c3}
+                            <div/> 
+                            D: {examsQuestion.c4}
+                            </Stack>
+                            <form onSubmit={handleSubmit}>
+                            <Stack marginTop={2} marginLeft={2}>
                         <FormControl>
                                 <FormLabel id="demo-radio-buttons-group-label">Answer</FormLabel>
                                 <RadioGroup
                                     aria-labelledby="demo-radio-buttons-group-label"
-                                    name="radio-buttons-group"
+                                    name="answer"
+                                     //value={values}
+                                    // onChange={handleRadioChange}
                                 >
+                                    <Stack marginTop={0.5} direction={"row"}>
+                                    
                                     <FormControlLabel value="A" control={<Radio />} label="A" name="A"/>
                                     <FormControlLabel value="B" control={<Radio />} label="B" name="B"/>
                                     <FormControlLabel value="C" control={<Radio />} label="C" name="C"/>
                                     <FormControlLabel value="D" control={<Radio />} label="D" name="D"/>
-                                </RadioGroup>
+        
+                                    </Stack>
+                                </RadioGroup> 
                             </FormControl>
+                            </Stack>
                             </form>
+                            
+                        </Grid>
                             </>
                     ))}
                 </Grid>
             </Stack>
-            <Button variant="contained"
-                onClick={() => {
-                    alert('clicked');
-                }}
-            >
+            <Stack marginTop={4}></Stack>
+            <Button variant="contained" href="/results">
                 Submit Answers
             </Button>
+            
             <p/>
             <Footer></Footer>
         </>
