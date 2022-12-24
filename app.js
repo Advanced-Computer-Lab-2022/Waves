aeabddea34b67f6b467e327bfd410091bb581a0d
Adam Samy <83244653+Adam2431@users.noreply.github.com>
2022-12-24 02:44:31 +0200

    Improved Login And Sign Up Pages

--- app.js
+++ app.js
@@ -13,11 +13,10 @@ console.log(process.env.ATLAS_URI);
 mongoose.connect(process.env.ATLAS_URI);
 
 app.use(bodyParser.urlencoded({
-  extended: true,
-  limit: '5mb'
+  extended: true
 }));
-app.use(express.json({limit: '5mb'}));
-app.use(express.urlencoded({limit: '5mb'}));
+app.use(express.json());
+app.use(express.urlencoded());
 
 
 app.use(session({
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,73 +1,166 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from '../components/navbar';
-import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 import {
-    BrowserRouter as Router,
-    Route,
-    useNavigate,
-    Link
-} from "react-router-dom";
-import { TextField } from '@mui/material';
-import Container from '@mui/material/Container';
+  Button,
+  Stack,
+  TextField,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
+import React from "react";
+import ResponsiveNavBar from "../components/ResponsiveNavBar";
+import Footer from "../components/Footer";
 
+const customRed = "rgb(180,40,40)";
 
+const theme = createTheme({
+  status: {
+    danger: "rgb(200,25,25)",
+  },
+  palette: {
+    primary: {
+      main: customRed,
+      darker: "#053e85",
+    },
+    secondary: {
+      main: "rgb(255,255,255)",
+      darker: "rgb(255,255,255)",
+    },
+    neutral: {
+      main: "#64748B",
+      contrastText: "#fff",
+    },
+  },
+});
 
 const Login = (props: any) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault();
-        const userInfo = new FormData(e.target)
-        const username = Object.fromEntries(userInfo.entries()).username
-        const password = Object.fromEntries(userInfo.entries()).password
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-        axios.post('http://localhost:3001/login', {
-            username: username,
-            password: password
-        }, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            withCredentials: true,
-        })
-            .then(response => {
-                navigate("../" + response.data);
-            });
-    }
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
 
-    return (
-        <>
-            <Navbar>
-            </Navbar>
-            <div className="container">
-                <div className="row g-2 m-3">
-                    <form onSubmit={handleSubmit}>
-                        <div>
-                            <div className="p-2">
-                            </div>
-                            <div className="p-2">
-                                <TextField id="outlined-basic" label="Username" variant="outlined" name="username" />
-                            </div>
-                        </div>
-                        <div>
-                            <div className="p-2">
+    axios
+      .post(
+        "http://localhost:3001/login",
+        {
+          username: username,
+          password: password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          withCredentials: true,
+        }
+      )
+      .then((response) => {
+        navigate("../" + response.data);
+      });
+  };
 
-                            </div>
-                            <div className="p-2">
-                                <TextField id="outlined-basic" label="Password" variant="outlined" name="password"  type='password'/>
-                            </div>
-                        </div>
-                        <div>
-                        <Link to="/reset-password" className="link" color="#f5f5f5">
-                            forgot password
-                        </Link>
-                        </div>
-                        <button className="ms-2 mt-1 btn btn-primary">Login</button>
-                    </form>
-                </div>
+  return (
+    <ThemeProvider theme={theme}>
+      <ResponsiveNavBar
+        pages={["All Course", "About Us"]}
+        isNotLoggedIn={true}
+      />
+      <div>
+        <div
+          className="grad"
+          style={{
+            minHeight: "100vh",
+            display: "flex",
+          }}
+        >
+          <Stack
+            marginLeft={"auto"}
+            marginRight={"auto"}
+            marginTop={"auto"}
+            marginBottom={"auto"}
+            spacing={1}
+          >
+            <img
+              style={{
+                width: 90,
+                height: 120,
+                marginRight: "auto",
+                marginLeft: "auto",
+              }}
+              src={"../AlienwareLogo.png"}
+            />
+            <Typography
+              variant="h6"
+              noWrap
+              sx={{
+                mr: 1,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 1000,
+                fontSize: 60,
+                letterSpacing: ".4rem",
+                color: "white",
+                textDecoration: "none",
+              }}
+            >
+              Alien Learning
+            </Typography>
+            <div
+              style={{
+                backgroundColor: "rgb(175,175,175)",
+                border: "solid 1.5px",
+                borderRadius: "1rem",
+                width: "30rem",
+                height: "35rem",
+                marginLeft: "auto",
+                marginRight: "auto",
+                paddingTop: "5.5rem",
+                padding: "3rem",
+                boxShadow: "2px 2px 2px 2px rgb(70,70,70)",
+              }}
+            >
+              <Stack
+                marginLeft={"auto"}
+                marginRight={"auto"}
+                marginTop={"auto"}
+                marginBottom={"auto"}
+                style={{ width: "20rem" }}
+                spacing={4}
+              >
+                <h2
+                  style={{
+                    textAlign: "center",
+                    textShadow: "2px 2px rgb(200,200,200)",
+                  }}
+                >
+                  Login
+                </h2>
+                <TextField
+                  label="Username"
+                  value={username}
+                  onChange={(event) => setUsername(event.target.value)}
+                ></TextField>
+                <TextField
+                  label="Password"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                ></TextField>
+                <Link to="/reset-password">Forgot Password</Link>
+                <Button variant="contained" onClick={handleSubmit}>
+                  Login
+                </Button>
+              </Stack>
             </div>
-        </>
-    )
-}
+          </Stack>
+        </div>
+        <Footer />
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export default Login;
